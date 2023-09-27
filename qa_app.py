9b5e08b37dda4007d92b068eba5c44a3f64e1993
Ifham Ansari <136684749+Ifham-Ansari@users.noreply.github.com>
2023-09-27 23:54:31 +0500

    updated

--- qa_app.py
+++ qa_app.py
@@ -189,20 +189,20 @@ def main():
     
 
 # Function to handle the "New Chat" button click
-def start_new_chat():
-    # Add your code to start a new chat here
-    print("New Chat started")
+# def start_new_chat():
+#     # Add your code to start a new chat here
+#     print("New Chat started")
 
-# Create the main application window
-root = tk.Tk()
-root.title("Chat App")
+# # Create the main application window
+# root = tk.Tk()
+# root.title("Chat App")
 
-# Create a button widget for "New Chat"
-new_chat_button = tk.Button(root, text="New Chat", command=start_new_chat)
-new_chat_button.pack()
+# # Create a button widget for "New Chat"
+# new_chat_button = tk.Button(root, text="New Chat", command=start_new_chat)
+# new_chat_button.pack()
 
-# Start the GUI event loop
-root.mainloop()
+# # Start the GUI event loop
+# root.mainloop()
 
 
     if 'openai_api_key' not in st.session_state:
