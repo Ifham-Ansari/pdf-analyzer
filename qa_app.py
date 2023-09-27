34db30762b75b497c455a2b0458be3b6ee973291
Ifham Ansari <136684749+Ifham-Ansari@users.noreply.github.com>
2023-09-27 22:32:02 +0500

    updated

--- qa_app.py
+++ qa_app.py
@@ -41,38 +41,38 @@ def load_docs(files):
 
 
 @st.cache_resource
-def create_retriever(_embeddings, splits, retriever_type):
-    if retriever_type == "SIMILARITY SEARCH":
-        try:
-            vectorstore = FAISS.from_texts(splits, _embeddings)
-        except (IndexError, ValueError) as e:
-            st.error(f"Error creating vectorstore: {e}")
-            return
-        retriever = vectorstore.as_retriever(k=5)
-    elif retriever_type == "SUPPORT VECTOR MACHINES":
-        retriever = SVMRetriever.from_texts(splits, _embeddings)
+# def create_retriever(_embeddings, splits, retriever_type):
+#     if retriever_type == "SIMILARITY SEARCH":
+#         try:
+#             vectorstore = FAISS.from_texts(splits, _embeddings)
+#         except (IndexError, ValueError) as e:
+#             st.error(f"Error creating vectorstore: {e}")
+#             return
+#         retriever = vectorstore.as_retriever(k=5)
+#     elif retriever_type == "SUPPORT VECTOR MACHINES":
+#         retriever = SVMRetriever.from_texts(splits, _embeddings)
 
-    return retriever
+#     return retriever
 
 @st.cache_resource
-def split_texts(text, chunk_size, overlap, split_method):
+# def split_texts(text, chunk_size, overlap, split_method):
 
-    # Split texts
-    # IN: text, chunk size, overlap, split_method
-    # OUT: list of str splits
+#     # Split texts
+#     # IN: text, chunk size, overlap, split_method
+#     # OUT: list of str splits
 
-    st.info("`Splitting doc ...`")
+#     st.info("`Splitting doc ...`")
 
-    split_method = "RecursiveTextSplitter"
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=overlap)
+#     split_method = "RecursiveTextSplitter"
+#     text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size=chunk_size, chunk_overlap=overlap)
 
-    splits = text_splitter.split_text(text)
-    if not splits:
-        st.error("Failed to split document")
-        st.stop()
+#     splits = text_splitter.split_text(text)
+#     if not splits:
+#         st.error("Failed to split document")
+#         st.stop()
 
-    return splits
+#     return splits
 
 @st.cache_data
 def generate_eval(text, N, chunk):
@@ -81,21 +81,21 @@ def generate_eval(text, N, chunk):
     # IN: text, N questions, chunk size to draw question from in the doc
     # OUT: eval set as JSON list
 
-    st.info("`Generating sample questions ...`")
-    n = len(text)
-    starting_indices = [random.randint(0, n-chunk) for _ in range(N)]
-    sub_sequences = [text[i:i+chunk] for i in starting_indices]
-    chain = QAGenerationChain.from_llm(ChatOpenAI(temperature=0))
-    eval_set = []
-    for i, b in enumerate(sub_sequences):
-        try:
-            qa = chain.run(b)
-            eval_set.append(qa)
-            st.write("Creating Question:",i+1)
-        except:
-            st.warning('Error generating question %s.' % str(i+1), icon="⚠️")
-    eval_set_full = list(itertools.chain.from_iterable(eval_set))
-    return eval_set_full
+    # st.info("`Generating sample questions ...`")
+    # n = len(text)
+    # starting_indices = [random.randint(0, n-chunk) for _ in range(N)]
+    # sub_sequences = [text[i:i+chunk] for i in starting_indices]
+    # chain = QAGenerationChain.from_llm(ChatOpenAI(temperature=0))
+    # eval_set = []
+    # for i, b in enumerate(sub_sequences):
+    #     try:
+    #         qa = chain.run(b)
+    #         eval_set.append(qa)
+    #         st.write("Creating Question:",i+1)
+    #     except:
+    #         st.warning('Error generating question %s.' % str(i+1), icon="⚠️")
+    # eval_set_full = list(itertools.chain.from_iterable(eval_set))
+    # return eval_set_full
 
 
 # ...
